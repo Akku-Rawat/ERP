e86250d3f7b5a034f08f4f280caa1f49dcd4bd78
dixantnegi001 <DIXANT.NEGI@ROLAFACE.COM>
2026-02-23 18:33:55 +0530

    refactor company tax

--- src/taxconfig/indiaTaxConfig.ts
+++ src/taxconfig/indiaTaxConfig.ts
@@ -1,32 +1,20 @@
 export const INDIA_TAX_CONFIGS = {
-  GST_0: {
-    taxType: "GST",
+  "Non-Export": {
+    taxType: "Standard Rated",
+    taxPerct: "16",
+    taxCode: "A",
+    taxDescription: "Applies to products subject to VAT at 16%.",
+  },
+  LPO: {
+    taxType: "Zero-Rated",
     taxPerct: "0",
-    taxCode: "GST0",
-    taxDescription: "Nil rated goods/services",
+    taxCode: "C2",
+    taxDescription: "Tax exempt transactions.",
   },
-  GST_5: {
-    taxType: "GST",
-    taxPerct: "5",
-    taxCode: "GST5",
-    taxDescription: "5% GST slab",
-  },
-  GST_12: {
-    taxType: "GST",
-    taxPerct: "12",
-    taxCode: "GST12",
-    taxDescription: "12% GST slab",
-  },
-  GST_18: {
-    taxType: "GST",
-    taxPerct: "18",
-    taxCode: "GST18",
-    taxDescription: "18% GST slab",
-  },
-  GST_28: {
-    taxType: "GST",
-    taxPerct: "28",
-    taxCode: "GST28",
-    taxDescription: "28% GST slab",
+  Export: {
+    taxType: "Export",
+    taxPerct: "0",
+    taxCode: "C1",
+    taxDescription: "Export outside country.",
   },
 };
